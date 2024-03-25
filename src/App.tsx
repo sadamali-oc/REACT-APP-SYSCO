a3fc8ef2f243c5e69f718f16e6f0d4764853a445
Chamalka Obadage <sadamalioc.21@uom.lk>
2024-03-26 00:57:22 +0530

    alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import ListGroup from "./components/ListGroup";
-function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+import Alert from "./components/Alert";
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+// import ListGroup from "./components/ListGroup";
+function App() {
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
   return (
     <div>
+      {/* 
       <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
       />
+ */}
+      <Alert text="Hello World" />
     </div>
   );
 }
